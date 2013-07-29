e8dba8a643b967abf7433b3035799bfb30a1f5bd
Siting <sophie.cst6@gmail.com>
2013-07-29 15:27:39 -0500

    new series 42

--- Configurations/set_parameters/setParameter42.m
+++ Configurations/set_parameters/setParameter42.m
@@ -45,6 +45,6 @@ PARAMETER.trueNodeRatioVar = 0;
 PARAMETER.modelFirst = 1;    % for model selection function
 PARAMETER.modelLast = 1;     % for model selection function
 PARAMETER.populationSize = 100;
-PARAMETER.samplingSize = 100;
-PARAMETER.thresholdVector = [3.5 3.5; 50 50; 50 50; 50 50; 50 50;...
-    50 50; 50 50; 50 50; 50 50; 50 50];
+PARAMETER.samplingSize = 80;
+PARAMETER.thresholdVector = [1 1; 50 50; 50 50; 50 50; 50 50;...
+    50 50; 50 50; 50 50;];
